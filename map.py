905213dfd3b5059625d11af4460c223d7685069b
= <=>
2023-08-02 22:50:28 -0500

    Update 1

--- map.py
+++ map.py
@@ -2,8 +2,14 @@ class Solution(object):
     def letterCombinations(self, digits):
         self.digits = digits
 
+    def mapping(self):
+        maps = {
+            1: '',
+            2: ['a', 'b', 'c']
+                }
 
 
 def main():
     sol = Solution()
     
+main()
